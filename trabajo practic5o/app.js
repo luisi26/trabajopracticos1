f5745539eddce203b67238a0ce518578f20bfa33
alumno <alumno@Instituto.local>
2026-06-04 08:14:13 -0300

    cambio
    4/6

--- trabajo practic5o/app.js
+++ trabajo practic5o/app.js
@@ -1,8 +1,53 @@
 let number1 = 25 ;
 let number2 =  23 ;
 let resultado
+let suma
+let resta
+let mul
+let division
 
-resultado = number1 + number2
+//mul
+mul = number1 *number2;
 
 console.log(resultado);
-alert(resultado)
+
+ //division
+ division= number1 / number2;
+
+ console.log(resultado);
+
+ //resta
+ resta = number1 - number2;
+ 
+
+ alert('resultado de restar '+number1 +' y '+ number2 +' es '+ resta);
+
+ //suma
+ suma = number1 + number2;
+console.log(suma)
+ alert(suma);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
