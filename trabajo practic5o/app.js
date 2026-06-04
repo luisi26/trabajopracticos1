748a804968968a7f98339d674b82972d24976920
alumno <52178187@ivt.edu.ar>
2026-06-04 09:07:24 -0300

    4/6

--- trabajo practic5o/app.js
+++ trabajo practic5o/app.js
@@ -5,16 +5,23 @@ let suma
 let resta
 let mul
 let division
+let apellido = 'lopez'
+let nombre = 'josefina'
+let edad = 14;
 
 //mul
 mul = number1 *number2;
 
 console.log(resultado);
 
+alert('resultado de restar '+number1 +' y '+ number2 +' es '+ mul);
+
  //division
  division= number1 / number2;
 
  console.log(resultado);
+
+ alert('resultado de la division '+number1 +' y '+ number2 +' es '+ division);
 
  //resta
  resta = number1 - number2;
@@ -25,9 +32,11 @@ console.log(resultado);
  //suma
  suma = number1 + number2;
 console.log(suma)
- alert(suma);
 
+ alert('resultado de la suma '+number1 +' y '+ number2 +' es '+ suma);
 
+ //nombre
+  console.log('el alumno' + nombre  +  apellido + ' tiene '+ edad)
 
 
 
