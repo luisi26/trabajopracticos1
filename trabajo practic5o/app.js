3a6ca63225039041d6c060b17f3c8aa088731014
Lari0801 <52178187@ivt.edu.ar>
2026-06-08 22:54:32 -0300

    070626

--- trabajo practic5o/app.js
+++ trabajo practic5o/app.js
@@ -8,55 +8,51 @@ let division
 let apellido = 'lopez'
 let nombre = 'josefina'
 let edad = 14;
+let precio1 = 10;
+let precio2 = 5;
+let precio3 = 1;
+let total = precio1 + precio2 + precio3;
+let comidafav = 'sorrentinos';
+let colorfav = 'violeta';
+let númerofav = 6;
+let pais = 'Argentina';
 
-//mul
-mul = number1 *number2;
+//suma
+suma = number1 + number2;
 
-console.log(resultado);
 
-alert('resultado de restar '+number1 +' y '+ number2 +' es '+ mul);
-
- //division
- division= number1 / number2;
-
- console.log(resultado);
-
- alert('resultado de la division '+number1 +' y '+ number2 +' es '+ division);
+alert(' el resultado de sumar '+ number1 +' y ' + number2 + ' es '+ suma);
 
  //resta
- resta = number1 - number2;
- 
+ Resta = number1 - number2;
+ alert(' el resultado de restar '+ number1 +' y ' + number2 + ' es '+ Resta);
 
- alert('resultado de restar '+number1 +' y '+ number2 +' es '+ resta);
+ //multiplicación
+ multiplicación = number1 * number2;
 
- //suma
- suma = number1 + number2;
-console.log(suma)
 
- alert('resultado de la suma '+number1 +' y '+ number2 +' es '+ suma);
+console.log(' el resultado de multiplicar '+ number1 +' y ' + number2 + ' es '+ multiplicación);
+
+//división
+división = number1 / number2;
+
+
+console.log(' el resultado de dividir '+ number1 +' y ' + number2 + ' es '+ división);
 
  //nombre
-  console.log('el alumno' + nombre  +  apellido + ' tiene '+ edad)
+  console.log (' la alumna '+  nombre +' '+  apellido + ' tiene '+ edad +' '+ 'años' )
+
+  //precios
+
+console.log ('el precio total a pagar es:'+ ' '+' $'+ (precio1 + precio2 + precio3))
 
 
+//Cosas favoritas
+console.log ('Mi comida favorita es'+' '+ comidafav+',' +'me gusta el color'+ ' '+ colorfav+' '+'y mi número favorito es el'+' '+ númerofav )
 
+//País
+console.log(pais)
+pais = "Brasil"
+console.log(pais);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//Luisina Calcavecchia
