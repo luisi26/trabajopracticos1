0906e7d021b8182d2ab337890b5d40d6ccb60184
alumno <52178187@ivt.edu.ar>
2026-06-25 09:05:26 -0300

    25/06

--- TP6/app.js
+++ TP6/app.js
@@ -1,3 +1,9 @@
-let boton = document.querySelector('button')
+let boton1 = document.querySelector ('#b1')
+let boton2 = document.querySelector ('#b2')
+let boton3 = document.querySelector ('#b3')
+let boton4 = document.querySelector ('#b4')
+let parrafo = document.querySelector ('#p1')
 
-boton.onclick = function ( )
+boton1.onclick = function(){
+    parrafo.style.backgroundColor ='blue'
+}
